53a3fbc08e9cc6936d61ec97c0bd4de37a2771c7
minhhung123 <prohungnguyenminh@gmail.com>
2022-11-06 16:34:50 +1100

    Change order provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { createClient, configureChains, defaultChains, WagmiConfig } from 'wagmi';
 import { extendTheme } from '@chakra-ui/react';
-import { publicProvider } from 'wagmi/providers/public';
-import { SessionProvider } from 'next-auth/react';
 import { WalletProvider } from 'components/chat/WalletProvider';
 import XmtpProvider from 'components/chat/XmtpProvider';
+import { publicProvider } from 'wagmi/providers/public';
+import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css'
 
@@ -26,17 +26,18 @@ const theme = extendTheme({ config });
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <WalletProvider>
-      <XmtpProvider>
+      <WalletProvider>
         <ChakraProvider resetCSS theme={theme}>
           <WagmiConfig client={client}>
             <SessionProvider session={pageProps.session} refetchInterval={0}>
-              <Component {...pageProps} />
+              <XmtpProvider>
+                <Component {...pageProps} />
+              </XmtpProvider>
             </SessionProvider>
           </WagmiConfig>
         </ChakraProvider>
-      </XmtpProvider>
-    </WalletProvider>
+      </WalletProvider>
+
   );
 };
 
