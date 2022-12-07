10194c0baf3f90a718482d1efd0872ba7dc34bbc
phuongdoan13 <mphuongdoant@gmail.com>
2022-12-07 21:57:17 +1100

    Add commented-out code for changing background colour

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,10 @@ import XmtpProvider from 'components/chat/XmtpProvider';
 import { publicProvider } from 'wagmi/providers/public';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
-import '../styles/globals.css'
-
+import { mode } from "@chakra-ui/theme-tools"
 const { provider, webSocketProvider } = configureChains(defaultChains, [publicProvider()]);
+import type { GlobalStyleProps } from "@chakra-ui/theme-tools"
+
 
 const client = createClient({
   provider,
@@ -17,10 +18,23 @@ const client = createClient({
 });
 
 const config = {
-  initialColorMode: 'dark',
+  initialColorMode: 'white',
   useSystemColorMode: false,
 };
 
+
+// const styles = {
+//   global: (props: GlobalStyleProps) => ({
+//     body: {
+//       fontFamily: 'body',
+//       color: mode('gray.800', 'whiteAlpha.900')(props),
+//       bg: mode('gray.800', 'whiteAlpha.900')(props),
+//       backgroundImage: mode('@public/unknown.png', '@public/unknown.png')(props),
+//       lineHeight: 'base',
+//     },
+    
+//   }),
+// }
 
 const theme = extendTheme({ config });
 
