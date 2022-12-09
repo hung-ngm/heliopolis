0e7cdfb4ddf9fdbe539adc76364f54a170634045
phuongdoan13 <mphuongdoant@gmail.com>
2022-12-09 19:51:20 +1100

    Small update on NFTImage component

--- src/components/modules/NFTImage/NFTImage.tsx
+++ src/components/modules/NFTImage/NFTImage.tsx
@@ -5,7 +5,7 @@ import { INFTImage } from './types';
 const NFTImage: FC<INFTImage> = ({ image }) => {
   return (
     <Box maxHeight="300px" overflow={'hidden'}>
-      <Image src={`${image}`} alt={'nft'} minH="300px" minW="300px" boxSize="100%" objectFit="contain" />
+      <Image src={`${image}`} alt={'nft'} minH="300px" minW="300px" boxSize="100%" objectFit="contain"/>
     </Box>
   );
 };
