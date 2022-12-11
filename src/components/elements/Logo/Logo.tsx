e8ade567660bc26bf5844927632d27518f8fcf80
phuongdoan13 <mphuongdoant@gmail.com>
2022-12-11 21:35:40 +1100

    Increase logo size to 3xl

--- src/components/elements/Logo/Logo.tsx
+++ src/components/elements/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import { Text, useColorModeValue} from '@chakra-ui/react';
 
 const Logo = () => {
   const logoColor = useColorModeValue('black', 'white');
-  return <Text as='b' fontSize={'xl'} color={logoColor}> HELIOPOLIS</Text>;
+  return <Text as='b' fontSize={'3xl'} color={logoColor}> HELIOPOLIS</Text>;
 };
 
 export default Logo;
