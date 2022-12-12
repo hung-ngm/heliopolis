97e6bac6e33261783fdefbae9d14764f28ca5194
phuongdoan13 <mphuongdoant@gmail.com>
2022-12-12 22:12:01 +1100

    Remove Home page, redirect to marketplace

--- src/components/elements/navigation/NavBar/paths.ts
+++ src/components/elements/navigation/NavBar/paths.ts
@@ -1,7 +1,6 @@
 import { ISubNav } from '../SubNav/SubNav';
 
 const NAV_LINKS: ISubNav[] = [
-  { label: 'Home', href: '/' },
   { label: 'Chat', href: '/chat' },
   { label: 'Marketplace', href: '/marketplace/explore' },
   { label: 'Your collection', href: '/marketplace/collection' },
