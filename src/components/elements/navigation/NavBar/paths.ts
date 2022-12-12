7ef577d82964daf1239b7e89d14c9814c999ac75
phuongdoan13 <mphuongdoant@gmail.com>
2022-12-12 22:14:17 +1100

    Rearrange element on NavBar

--- src/components/elements/navigation/NavBar/paths.ts
+++ src/components/elements/navigation/NavBar/paths.ts
@@ -1,7 +1,6 @@
 import { ISubNav } from '../SubNav/SubNav';
 
 const NAV_LINKS: ISubNav[] = [
-  { label: 'Chat', href: '/chat' },
   { label: 'Marketplace', href: '/marketplace/explore' },
   { label: 'Your collection', href: '/marketplace/collection' },
   {
@@ -19,9 +18,10 @@ const NAV_LINKS: ISubNav[] = [
         subLabel: 'Use Dall-E to create your own NFT',
         href: '/create/dalle',
         logo: 'wizard',
-      }
-    ]
-  }
+      },
+    ],
+  },
+  { label: 'Chat', href: '/chat' },
 ];
 
 export default NAV_LINKS;
