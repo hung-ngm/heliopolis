da970afe2a852c330e773c973ea559e8dbeaa9a6
phuongdoan13 <mphuongdoant@gmail.com>
2022-12-14 09:49:02 +1100

    Add NFTImage hover effect

--- src/components/modules/NFTImage/NFTImage.tsx
+++ src/components/modules/NFTImage/NFTImage.tsx
@@ -5,7 +5,7 @@ import { INFTImage } from './types';
 const NFTImage: FC<INFTImage> = ({ image }) => {
   return (
     <Box maxHeight="300px" overflow={'hidden'}>
-      <Image src={`${image}`} alt={'nft'} minH="300px" minW="300px" boxSize="100%" objectFit="contain"/>
+      <Image src={`${image}`} alt={'nft'} minH="300px" minW="300px" boxSize="100%" objectFit="contain" _hover={{ transform: "scale(1.2)", }} transition={"0.2s ease-in-out"}/>
     </Box>
   );
 };
