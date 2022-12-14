263217af56aa30cc6cb0d59c6b76669912055289
phuongdoan13 <mphuongdoant@gmail.com>
2022-12-14 20:44:59 +1100

    Change NFTCollectionModal to vertical

--- src/components/modules/NFTCollectionModal/NFTCollectionModal.tsx
+++ src/components/modules/NFTCollectionModal/NFTCollectionModal.tsx
@@ -8,13 +8,13 @@ import {
   ModalCloseButton,
   Center,
   Button,
-  SimpleGrid,
   VStack,
   Image,
   Text,
   Box,
   Badge,
   useToast,
+  ToastId,
 } from '@chakra-ui/react';
 import { FormControl, FormLabel, FormErrorMessage, FormHelperText, Input } from '@chakra-ui/react';
 import { resellNft } from '@pages/api/nft/sellNft';
@@ -28,7 +28,7 @@ const NFTCollectionModal: FC<INFTCollectionModal> = ({ name, description, image,
   const initialRef = React.useRef(null);
   const finalRef = React.useRef(null);
   const toast = useToast();
-  const toastIdRef = React.useRef();
+  const toastIdRef = React.useRef<ToastId>();
 
   const updateToast = (e: string | null) => {
     if (toastIdRef.current) {
@@ -102,63 +102,63 @@ const NFTCollectionModal: FC<INFTCollectionModal> = ({ name, description, image,
 
   return (
     <>
-      <Modal initialFocusRef={initialRef} finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose} size={'4xl'}>
+      <Modal initialFocusRef={initialRef} finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose} size={'xl'}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>
-            <Center>
-              <VStack>
+          <ModalHeader/>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack>
+              <Center>
+                <Box maxWidth="500px" maxHeight="500px" overflow={'hidden'}>
+                  <Image
+                    src={`${image}`}
+                    alt={'nft'}
+                    minWidth={'400px'}
+                    minHeight={'400px'}
+                    boxSize="100%"
+                    objectFit="contain"
+                  />
+                </Box>
+              </Center>
+
+              <Center>
                 <Text fontSize={25} fontWeight={'bold'}>
                   {' '}
                   {name}{' '}
+                  <Badge fontWeight="bold" fontSize="20px" colorScheme={'green'}>
+                    #{tokenId}
+                  </Badge>
                 </Text>
-                <Badge fontWeight="bold" fontSize="20px" colorScheme={'green'}>
-                  #{tokenId}
-                </Badge>
-              </VStack>
-            </Center>
-          </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <SimpleGrid columns={2} marginLeft={'5px'} marginRight={'5px'}>
-              <VStack align={'left'} spacing={10}>
-                <FormControl isInvalid={isError}>
-                  <FormLabel fontWeight={'bold'}>Price (MATIC)</FormLabel>
-                  <Input
-                    isRequired
-                    ref={initialRef}
-                    value={input}
-                    onChange={handleInputChange}
-                    placeholder="Enter your price"
-                    width={{'lg':"300px", 'md':'200px', 'sm':'100px'}}
-                  />
-                  {!isError ? (
-                    <FormHelperText>*Only integer price</FormHelperText>
-                  ) : (
-                    <FormErrorMessage>*Enter a valid integer price</FormErrorMessage>
-                  )}
-                </FormControl>
-                <VStack align={'left'}>
-                  <Text as={'u'} fontWeight={'bold'}>
-                    Description
-                  </Text>
+              </Center>
+              <Center>
+                <Center>
                   <Text as={'i'} fontWeight={'normal'} fontSize={'20px'}>
                     {`${description}`}
                   </Text>
-                </VStack>
-              </VStack>
-
-              <Box maxWidth="500px" maxHeight="500px" overflow={'hidden'}>
-                <Image
-                  src={`${image}`}
-                  alt={'nft'}
-                  minWidth={'400px'}
-                  minHeight={'400px'}
-                  boxSize="100%"
-                  objectFit="contain"
-                />
-              </Box>
-            </SimpleGrid>
+                </Center>
+              </Center>
+              <Center>
+                <Box>
+                  <FormControl isInvalid={isError}>
+                    <FormLabel fontWeight={'bold'}>Price (MATIC)</FormLabel>
+                    <Input
+                      isRequired
+                      ref={initialRef}
+                      value={input}
+                      onChange={handleInputChange}
+                      placeholder="Enter your price"
+                      width={{ lg: '400px', md: '400px', sm: '400px' }}
+                    />
+                    {!isError ? (
+                      <FormHelperText>*Only integer price</FormHelperText>
+                    ) : (
+                      <FormErrorMessage>*Enter a valid integer price</FormErrorMessage>
+                    )}
+                  </FormControl>
+                </Box>
+              </Center>
+            </VStack>
           </ModalBody>
 
           <ModalFooter>
@@ -172,7 +172,9 @@ const NFTCollectionModal: FC<INFTCollectionModal> = ({ name, description, image,
             >
               Sell
             </Button>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>Cancel</Button>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
