681b4cca122f58cb94229c9f180ef48b2a0c408a
phuongdoan13 <mphuongdoant@gmail.com>
2022-12-19 11:02:25 +1100

    Handle error in sellNFT

--- pages/api/nft/sellNft.ts
+++ pages/api/nft/sellNft.ts
@@ -26,7 +26,7 @@ export const resellNft = async (nft: any) => {
         console.log(response);
         
     } catch (e:any){
-        console.log(e);
+        throw new Error("Unexpected Error from the network. Consider increase your gas (Max Priority and Max Fee > 30) or try again.");
     }
 
 }
