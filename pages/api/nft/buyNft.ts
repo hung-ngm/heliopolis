743173c3eab8e3aab7deac76bfa844d5a903b35c
phuongdoan13 <mphuongdoant@gmail.com>
2022-12-19 11:04:07 +1100

    Change error prompt in buyNFT

--- pages/api/nft/buyNft.ts
+++ pages/api/nft/buyNft.ts
@@ -24,7 +24,7 @@ export const buyNft = async (nft: any) => {
         
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     }catch (e: any){
-        throw new Error("Unexpected Error from the network. Consider increase your gas or try again.");
+        throw new Error("Unexpected Error from the network. Consider increase your gas (Max Priority and Max Fee > 30) or try again.");
     }
 
 }
